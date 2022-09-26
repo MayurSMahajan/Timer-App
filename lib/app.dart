b30ee0b18ac27f624ae88d3045487e5e1be29836
Shreyash Rachchawar <mayur23smahajan@gmail.com>
2022-09-26 20:20:25 +0530

    Initial Commit

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:timer_app/timer/timer.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timer',
+      theme: ThemeData(
+          primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+          colorScheme: const ColorScheme.light(
+            secondary: Colors.indigo,
+          )),
+      home: const TimerPage(),
+    );
+  }
+}
